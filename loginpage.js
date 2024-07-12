7aac1ec4be3ddedcfda671950f983e8592df2658
AyeshaNaveed <ayesha.naveed3305@gmail.com>
2024-07-12 15:41:12 +0500

    first commit

---
+++ loginpage.js
@@ -0,0 +1,58 @@
+const mysql = require('mysql2');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+var pool = mysql.createPool({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '1234',
+    database: 'pagess',
+}).promise();
+
+app.get('/users', async (req, resp) => {
+    const [rows] = await pool.query('SELECT * FROM login');
+    resp.json(rows);
+});
+
+app.post('/user/login', async (req, resp) => {
+    const { email, password } = req.body;
+
+    const [rows] = await pool.query('SELECT * FROM login WHERE email = ? and password = ?', [email, password]);
+
+    if (rows.length > 0) {
+        resp.send('Login successful');
+    } else {
+        resp.send('Email and password not found');
+    }
+});
+
+//for signup
+app.get('/signupusers', async (req, resp) => {
+    const [rows] = await pool.query('SELECT * FROM signup');
+    resp.json(rows);
+});
+
+app.post('/signup', async (req, resp) => {
+    const { name, pasword, email } = req.body;
+    const result = await pool.query('INSERT INTO signup (name, pasword, email) VALUES (?, ?, ?)', [name, pasword, email]);
+    resp.json({ id: result.insertId, name, pasword, email });
+});
+
+app.put('/update/:id',async(req,res)=>{
+    const id = req.params.id;
+    const { name, email, pasword} = req.body;
+    const result = await pool.query('update signup set name=?,email=?,pasword=? where id=?', [name, email, pasword,id]);
+    res.json({ id: result.insertId, name, email, pasword });
+})
+
+app.delete('/delete/:id',async(req,res)=>{
+    const id = req.params.id;
+    const result = await pool.query('delete from signup  where id=?', [id]);
+    res.json({message:'deleted successfully' });
+})
+
+
+
+app.listen(1500);
