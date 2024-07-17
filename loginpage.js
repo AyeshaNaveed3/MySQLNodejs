aaaa39de5dee7d91954d3e5311fcd0d4658e2daa
AyeshaNaveed <ayesha.naveed3305@gmail.com>
2024-07-17 15:41:23 +0500

    added jwt token

--- loginpage.js
+++ loginpage.js
@@ -1,6 +1,8 @@
 const mysql = require('mysql2');
 const express = require('express');
 const app = express();
+const jwt = require('jsonwebtoken');
+const secretkey = 'secretkey';
 
 app.use(express.json());
 
@@ -22,7 +24,8 @@ app.post('/user/login', async (req, resp) => {
     const [rows] = await pool.query('SELECT * FROM login WHERE email = ? and password = ?', [email, password]);
 
     if (rows.length > 0) {
-        resp.send('Login successful');
+        const token = jwt.sign(rows[0], secretkey, { expiresIn: '5d' });
+        resp.send({ token, user: rows[0] })
     } else {
         resp.send('Email and password not found');
     }
@@ -40,18 +43,46 @@ app.post('/signup', async (req, resp) => {
     resp.json({ id: result.insertId, name, pasword, email });
 });
 
-app.put('/update/:id',async(req,res)=>{
+app.put('/update/:id', async (req, res) => {
     const id = req.params.id;
-    const { name, email, pasword} = req.body;
-    const result = await pool.query('update signup set name=?,email=?,pasword=? where id=?', [name, email, pasword,id]);
+    const { name, email, pasword } = req.body;
+    const result = await pool.query('update signup set name=?,email=?,pasword=? where id=?', [name, email, pasword, id]);
     res.json({ id: result.insertId, name, email, pasword });
 })
 
-app.delete('/delete/:id',async(req,res)=>{
+app.delete('/delete/:id', async (req, res) => {
     const id = req.params.id;
     const result = await pool.query('delete from signup  where id=?', [id]);
-    res.json({message:'deleted successfully' });
+    res.json({ message: 'deleted successfully' });
 })
+
+app.post('/profile', verifytoken, (req, resp) => {
+    jwt.verify(req.token, secretkey, (err, authData) => {
+        if (err) {
+            resp.send({ result: 'inavlid token' });
+        }
+        else {
+            resp.json({
+                message: 'profile accessed',
+                authData: authData
+
+            })
+        }
+    })
+})
+
+function verifytoken(req, resp, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(" ");
+        const token = bearer[1];
+        req.token = token;
+        next();
+    }
+    else {
+        resp.send({ result: 'token is not valid' })
+    }
+}
 
 
 
